52b850f3b2b32e6fcf92b3007f05e1eee324ba5d
Oleksandr Kravets <oleksandrkravets1995@gmail.com>
2020-02-24 16:38:49 +0200

    Fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AnnouncementComponent } from './components/announcement/announcement.component';
 import { InfoComponent } from './components/info/info.component';
-import {LoginComponent} from "./components/login/login.component";
-import {RegistrationComponent} from "./components/registration/registration.component";
-import {AnnouncementDetailsComponent} from "./components/announcement-details/announcement-details.component";
-import {AddAnnouncementComponent} from "./components/add-announcement/add-announcement.component";
+import {LoginComponent} from './components/login/login.component';
+import {RegistrationComponent} from './components/registration/registration.component';
+import {AnnouncementDetailsComponent} from './components/announcement-details/announcement-details.component';
+import {AddAnnouncementComponent} from './components/add-announcement/add-announcement.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'announcement', pathMatch: 'full' },
--- src/app/components/add-announcement/add-announcement.component.ts
+++ src/app/components/add-announcement/add-announcement.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ExistUserHttpService} from "../../services/existUser-http.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 
 @Component({
@@ -16,9 +17,26 @@ export class AddAnnouncementComponent implements OnInit {
     description: ''
   };
 
+  pets = [
+    {id: 1, name: "Cat"},
+    {id: 2, name: "Dog"},
+    {id: 3, name: "Fish"},
+    {id: 4, name: "Parrot"}
+  ];
+  selectedPet = null;
+  submitted = false;
+  form: FormGroup;
+
   constructor(private httpUser: ExistUserHttpService) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl('', Validators.required),
+      price: new FormControl('', Validators.required),
+      location: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required),
+      image: new FormControl('', Validators.required)
+    });
   }
 
   createAnnouncement() {
@@ -28,7 +46,7 @@ export class AddAnnouncementComponent implements OnInit {
       location: this.announcement.location,
       images: [{image:  this.announcement.image}],
       description: this.announcement.description,
-      sections_id: 1,
+      sections_id: this.selectedPet,
       user_id: 8,
       status: 'Not VERIFY'
     };
@@ -41,6 +59,20 @@ export class AddAnnouncementComponent implements OnInit {
         error => {
           console.log(error);
         });
+
+    this.submitted = true;
+  }
+
+  cleanAnnouncementForm() {
+    this.announcement = {
+      title: '',
+      price: '',
+      location: '',
+      image: '',
+      description: ''
+    };
+    this.selectedPet = null;
+    this.submitted = false;
   }
 
 }
